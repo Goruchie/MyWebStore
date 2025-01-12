6ebc5b97a381806147623415afa56dea6e8b8aba
Marcos Gualtero Lourenzo <marcosgualtero43253@gmail.com>
2025-01-12 19:05:07 -0300

    Add Favorite class to Domain

---
+++ domain/Favorite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain
+{
+    public class Favorite
+    {
+        public string Id { get; set; }
+        public User User { get; set; }
+        public Item Item { get; set; }
+    }
+}
